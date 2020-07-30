65d5eb23fd85aabebb3e12416d7b2961c9304c6d
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-07-30 20:48:35 -0300

    DOckerizado a aplicacao para usar banco ms sql

---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+	public class ProdutosController : MainController
+	{
+		private readonly IMapper _mapper;
+		private readonly IProdutoRepository _produtoRepository;
+		private readonly IProdutoService _produtoService;
+
+		public ProdutosController(INotificador notificador,
+			IProdutoRepository produtoRepository,
+			IProdutoService produtoService,
+			IMapper mapper) : base(notificador)
+		{
+			_mapper = mapper;
+			_produtoRepository = produtoRepository;
+			_produtoService = produtoService;
+		}
+
+		[HttpGet]
+		public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+		{
+			return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosPorFornecedores());
+		}
+
+		[HttpGet("{id:guid}")]
+		public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+		{
+			var produtosViewModel = await ObterProduto(id);
+
+			if (produtosViewModel == null) return NotFound();
+
+			return produtosViewModel;
+		}
+
+		[HttpGet("{id:guid}")]
+		public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
+		{
+			var produtosViewModel = await ObterProduto(id);
+
+			if (produtosViewModel == null) return NotFound();
+
+			await _produtoService.Remover(id);
+
+			return CustomResponse(produtosViewModel);
+		}
+
+		public async Task<ProdutoViewModel> ObterProduto(Guid id)
+		{
+			return _mapper.Map<ProdutoViewModel>(await _produtoRepository.ObterProdutosPorFornecedor(id));
+		}
+	}
+}
