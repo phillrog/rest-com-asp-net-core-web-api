b6aeae89db3718706979e8b6ea456805552cb555
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-08-03 08:38:16 -0300

    Restringindo o acesso ao Swagger

--- src/DevIO.Api/Configurations/SwaggerConfig.cs
+++ src/DevIO.Api/Configurations/SwaggerConfig.cs
@@ -51,7 +51,8 @@ namespace DevIO.Api.Configuration
 
         public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
         {
-            app.UseSwagger();
+			//app.UseMiddleware<SwaggerAuthorizedMiddleware>();
+			app.UseSwagger();
             app.UseSwaggerUI(
                 options =>
                 {
@@ -136,4 +137,28 @@ namespace DevIO.Api.Configuration
             }
         }
     }
+
+	public class SwaggerAuthorizedMiddleware
+	{
+		private readonly RequestDelegate _next;
+
+		public SwaggerAuthorizedMiddleware(RequestDelegate next)
+		{
+			_next = next;
+		}
+
+		public async Task Invoke(HttpContext context)
+		{
+			if (context.Request.Path.StartsWithSegments("/swagger")
+				&& !context.User.Identity.IsAuthenticated)
+			{
+				context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+				return;
+			}
+
+			// redirecionar paga login
+
+			await _next.Invoke(context);
+		}
+	}
 }
